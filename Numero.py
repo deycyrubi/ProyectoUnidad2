19a46eeee0571b192d65fc6bda3424d00b59b472
DaycyPM <Deycy5julio@gmail.com>
2019-03-22 19:59:07 -0600

    Este es mi proyecto para la evaluación de la Unidad 2 de la materia de POO

---
+++ Numero.py
@@ -0,0 +1,36 @@
+"""se identifico el nombre de la clase"""
+class Numero:
+
+    # se identificaron los atributos
+    __NumeroIngresado: float(0)
+    __NumeroExponente: float(0)
+    __a: float(0)
+    __b: float(0)
+
+
+    # se importo la palabra reservada math para la resolución del problema
+    import math
+
+
+def __init__(self, NumeroIngresado):
+    """se identifico el metodo constructor"""
+    self.__NumeroIngresado = NumeroIngresado
+    self.NumeroExponente()
+
+a=0
+b=2
+def realizarNumeroExponente(self):
+    for a in range(0,2):
+         print(b,'^',a,' = '(pow(b**a)))
+
+#se declara el metodo el cual sera la parte lógica o de solución al problema
+def NumeroExponente(self):
+    self.__NumeroExponente = (self.__NumeroExponente)
+    self.__NumeroIngresado()
+
+def getNumeroExponente(self): #se usa get para que pueda mostrar el valor, puesto que  está declarado como privado
+
+    return self.__NumeroIngresado #return es una palabra reservada para devolver un valor
+
+
+
